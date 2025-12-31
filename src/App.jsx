a9633dd99e64ff286f8af2d861d2e0591dcee9df
seojun01 <parkseojoon@naver.com>
2025-12-31 22:31:08 +0900

    Main Setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
 import { useState } from "react";
 import "./App.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Button, Card, CardBody } from "react-bootstrap";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <>
+      <div className="App">
+        <Navbar bg="dark" data-bs-theme="dark">
+          <Container>
+            <Navbar.Brand href="#home">Dashboard</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src="" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>test card</Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      </div>
+    </>
+  );
 }
 
 export default App;
